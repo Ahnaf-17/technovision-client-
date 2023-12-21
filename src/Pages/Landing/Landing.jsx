9f3f6ea2998ae0de45302c4ae73bdd632cfbc67d
unknown <ahnaf.ahmed.42173@gmail.com>
2023-12-22 01:40:16 +0600

    aos added to users section and banner

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,16 +1,23 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
 import UsersCard from "./Users/UsersCard";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 const Landing = () => {
     const user = useLoaderData();
-    console.log(user.length);
+    useEffect(()=>{
+        AOS.init()
+    },[])
     return (
         <div className="max-w-screen-xl mx-auto">
             <Banner></Banner>
             <section>
-                <h3 className="text-center items-center text-white text-3xl font-bold">People Who Use</h3>
-            <div className="grid lg:grid-cols-2 grid-cols-1 gap-10 my-10 lg:mx-0 mx-4">
+                <h3 className="text-center items-center text-white text-3xl font-bold my-20" data-aos="fade-up"
+     data-aos-duration="2000">People Who Use</h3>
+            <div className="grid lg:grid-cols-2 grid-cols-1 gap-10 my-10 lg:mx-0 mx-4" data-aos="fade-up"
+     data-aos-duration="2000">
             {
                 user.map(aUser => <UsersCard key={user.id} users={aUser}></UsersCard>)
             }
--- src/Pages/Landing/Users/UsersCard.jsx
+++ src/Pages/Landing/Users/UsersCard.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable react/prop-types */
-
-const UsersCard = ({users}) => {
-    const {type,benefit} = users;
+import img1 from '../../../assets/banner.jpg'
+const UsersCard = ({ users }) => {
+    const { type, benefit } = users;
     return (
         <div>
-            <div className="flex flex-col w-full lg:flex-row">
-                <div className="grid flex-grow h-32 card bg-base-100 text-center rounded-box items-center">
-                    <div className="text-center">
-                    <h2>{type}</h2>
+            <div className="card bg-base-100 shadow-xl relative">
+                <img src={img1} alt="Shoes" className='w-full h-[124px] absolute opacity-30  rounded-2xl' />
+                <div className="card-body z-10">
+                    <h2 className="card-title text-2xl font-bold">{type}</h2>
                     <p>{benefit}</p>
-                    </div>
                 </div>
             </div>
         </div>
