58f8470c1d754174bf8d51da7aaa49c4bf95453a
unknown <ahnaf.ahmed.42173@gmail.com>
2023-12-21 15:30:24 +0600

    login complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,100 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../../Hooks/useAuth";
+import { useState } from "react";
+import Swal from "sweetalert2";
+import { FaGoogle } from "react-icons/fa";
+
 
 const Login = () => {
+    const { logIn, googleLogin } = useAuth()
+    const location = useLocation();
+    const navigate = useNavigate()
+    const [logInErr, setLogInErr] = useState('')
+
+    const handleLogin = e => {
+        e.preventDefault();
+        console.log(e.currentTarget)
+        const form = new FormData(e.currentTarget);
+        const email = form.get('email')
+        const password = form.get('password')
+        setLogInErr('')
+        logIn(email, password)
+            .then(result => {
+                navigate(location?.state ? location.state : '/')
+                // if(isParticipant){
+                    
+                // }
+                // else if(isOrganizer){
+                //     navigate(location?.state ? location.state : '/dashboard/organizer-profile')
+                // }
+                // else if(isProfessional){
+                //     navigate(location?.state ? location.state : '/dashboard/professional-profile')
+                // }
+                // if (isParticipant == true) {
+                //     navigate(location?.state ? location.state : '/dashboard/participant-profile');
+                // } else if (isOrganizer == true) {
+                //     navigate(location?.state ? location.state : '/dashboard/organizer-profile');
+                // } else if (isProfessional == true) {
+                //     navigate(location?.state ? location.state : '/dashboard/professional-profile');
+                // }
+            })
+            .catch(error => {
+                console.error(error)
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'incorrect password or email',
+                    footer: '<a href="">Why do I have this issue?</a>'
+                })
+            })
+    }
+    const handleGoogleLogin = () => {
+        googleLogin()
+            .then(result => {
+                console.log(result.user)
+                navigate(location?.state ? location.state : '/')
+
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
+    
     return (
-        <div>
-            login
+        <div className="hero min-h-screen bg-base-100">
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-cyan-800 md:w-3/4 lg:w-1/2 mx-auto rounded-xl">
+                <form onSubmit={handleLogin} className="card-body ">
+                    <div>
+                        <h2 className="text-3xl text-center text-white font-bold ">Please Login</h2>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-bold text-white">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="font-bold text-white label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" className="input input-bordered" name="password" required />
+
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn bg-neutral text-white font-bold">Login</button>
+                    </div>
+                    <div className="form-control text-center">
+                        <h2 className="text-white font-bold">Or</h2>
+                    </div>
+                    <div className="form-control mt-2">
+                        <button onClick={handleGoogleLogin} className="btn bg-white text-blue-700 font-extrabold">LogIn with Google <FaGoogle></FaGoogle></button>
+                    </div>
+                </form>
+                <p className="text-center pb-10 text-white mt-3">Don't have an account? <Link className="text-blue-300 font-bold" to='/register'>Register</Link></p>
+            </div>
         </div>
     );
 };
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -4,102 +4,125 @@ import useAuth from "../../Hooks/useAuth";
 import { useState } from "react";
 import Swal from "sweetalert2";
 import { updateProfile } from "firebase/auth";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
     const [regError, setRegError] = useState('')
     const [regSuccess, setRegSuccess] = useState('')
     const {createUser} = useAuth()
-    const handleRegister = e => {
-        e.preventDefault();
-        console.log(e.currentTarget)
-        const form = new FormData(e.currentTarget);
-        const name = form.get('name')
-        const imgUrl = form.get('image')
-        const email = form.get('email');
-        const password = form.get('password')
-        const upperCase = /[A-Z]/;
-        if(password <6 || !upperCase.test(password) ||!password.match(/[!"#$%&'()*+,-.:;<=>?@[\]^_`{|}~]/)){
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: "Minimum 6 character, 1 uppercase and one special character needed",
-                footer: '<a href="">Why do I have this issue?</a>'
-              })
-            return;
-        }
-        setRegError('')
-        setRegSuccess('')
-        createUser(email,password)
-        .then(result =>{
-            console.log(result.user)
-            Swal.fire(
-                'Good job!',
-                'You clicked the button!',
-                'success'
-              )
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const onSubmit = data => {
 
-            // update 
-            updateProfile(result.user, {
-                displayName:name,
-                photoURL:imgUrl
+        createUser(data.email, data.password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                updateProfile(data.name, data.photoURL)
+                    .then(() => {
+                        // create user entry in the database
+                        const userInfo = {
+                            name: data.name,
+                            email: data.email,
+                            role: data.role
+                        }
+                        // axiosPublic.post('/users', userInfo)
+                        //     .then(res => {
+                        //         if (res.data.insertedId) {
+                        //             console.log('user added to the database')
+                        //             reset();
+                        //             Swal.fire({
+                        //                 position: 'top-end',
+                        //                 icon: 'success',
+                        //                 title: 'User created successfully.',
+                        //                 showConfirmButton: false,
+                        //                 timer: 1500
+                        //             });
+                        //             navigate('/');
+                        //         }
+                        //     })
 
+                        console.log(result.user)
+                        Swal.fire(
+                            'Good job!',
+                            'You clicked the button!',
+                            'success'
+                          )
+            
+
+                    })
+                    .catch(error => console.log(error))
+                    // .catch(error =>{
+                    //     console.error(error);
+                    //     Swal.fire({
+                    //         icon: 'error',
+                    //         title: 'Oops...',
+                    //         text: 'incorrect password or email',
+                    //         footer: '<a href="">Why do I have this issue?</a>'
+                    //       })
+                    // })
             })
-            .then(()=> console.log("updated"))
-            .catch()
-        })
-        .catch(error =>{
-            console.error(error);
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'incorrect password or email',
-                footer: '<a href="">Why do I have this issue?</a>'
-              })
-        })
+    };
 
-
-    }
     return (
-        <div className=" min-h-screen bg-base-100">
+        <div className="hero min-h-screen bg-base-100">
 
-            <div className="my-20">
-                <form onSubmit={handleRegister} className="card-body bg-cyan-800 md:w-3/4 lg:w-1/2 mx-auto rounded-xl">
-                    <div>
-                        <h2 className="text-3xl text-center text-white font-bold ">Please Register</h2>
-                    </div>
-                    <div className="form-control">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-cyan-800 md:w-3/4 lg:w-1/2 mx-auto rounded-xl">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body ">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-white">Name</span>
+                                </label>
+                                <input type="text"  {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered " />
+                                {errors.name && <span className="text-red-600">Name is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-white">Photo URL</span>
+                                </label>
+                                <input type="text"  {...register("photoURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
+                                {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-white">Email</span>
+                                </label>
+                                <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
+                                {errors.email && <span className="text-red-600">Email is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-white">Password</span>
+                                </label>
+                                <input type="password"  {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 20,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                                })} placeholder="password" className="input input-bordered" />
+                                {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                                {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                                {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+                                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
+                            </div>
+                            <div className="form-control">
                         <label className="label">
-                            <span className="label-text font-bold text-white">Email</span>
+                            <span className="label-text text-white">Role</span>
                         </label>
-                        <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                        <select {...register("role", { required: true })} className="select select-bordered">
+                            <option value="">Select Role</option>
+                            <option value="participant">Participant</option>
+                            <option value="organizer">Organizer</option>
+                            <option value="professional">Professional</option>
+                        </select>
+                        {errors.role && <span className="text-red-600">Role is required</span>}
                     </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text font-bold text-white">Your Name</span>
-                        </label>
-                        <input type="text" placeholder="Your Name" name="name" className="input input-bordered" required />
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary" type="submit" value="Sign Up" />
+                            </div>
+                        </form>
+                        <p className="text-center mt-3 text-white pb-2">Already have an account,  <Link className="text-blue-400 font-semibold" to='/login'>Login</Link></p>
                     </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text font-bold text-white">Image url</span>
-                        </label>
-                        <input type="text" placeholder="image_url(optional)" name="image" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text font-bold text-white">Password</span>
-                        </label>
-                        <input type="password" placeholder="password" className="input input-bordered" name="password" required />
-
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn bg-neutral text-white font-bold">Register</button>
-                    </div>
-                </form>
-
-                <p className="text-center mt-3 text-black">Already have an account,  <Link className="text-blue-400 font-semibold" to='/login'>Login</Link></p>
             </div>
-        </div>
     );
 };
 
