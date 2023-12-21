e7843aec40274edbd54925bd740b44d82de2bfac
unknown <ahnaf.ahmed.42173@gmail.com>
2023-12-21 14:38:01 +0600

    authprovider created

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import getAuth, { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
+
+export const AuthContext = createContext()
+const auth = getAuth(app)
+
+const AuthProvider = ({children}) => {
+
+    const [user,setUser] = useState(null)
+    const [loading,setLoading] = useState(true);
+    const provider = new GoogleAuthProvider();
+
+    const createUser = (email,password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+    const logIn = (email,password) =>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+    const googleLogin = () =>{
+        setLoading(true)
+        return signInWithPopup(auth,provider)
+
+    }
+    const logOut = () =>{
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser=>{
+            console.log("user in auth", currentUser);
+            setUser(currentUser)
+            setLoading(false) 
+        })
+        return ()=>{
+            unSubscribe()
+        }
+    },[])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        logIn,
+        googleLogin,
+        logOut
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,46 +1,102 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from '../../assets/logo-png.png'
 
 const Navbar = () => {
+    const navLinks = <>
+    <li><NavLink to='/' className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "text-cyan-900 font-bold underline" : ""
+    }>Home</NavLink></li>
+
+    <li><NavLink to='/dashboard' className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "text-cyan-900 font-bold underline" : ""
+    }>Dashboard</NavLink></li>
+
+    <li><NavLink to='/contact' className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "text-cyan-900 font-bold underline" : ""
+    }>Contact Us</NavLink></li>
+
+    <li><NavLink to='/about' className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "text-cyan-900 font-bold underline" : ""
+    }>About</NavLink></li>
+
+
+    {/* <li><NavLink to='/login' className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "text-stone-500 font-bold underline" : ""
+    }>Login</NavLink></li>
+    <li><NavLink to='/register' className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "text-stone-500 font-bold underline" : ""
+    }>Register</NavLink></li> */}
+</>
     return (
         <>
-            <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
+            <div className="navbar  bg-base-100 text-neutral-content h-28 lg:mb-8">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn bg-base-100 border-none lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-black">
+                        {navLinks}
+                    </ul>
+                </div>
+
+                <Link to='/'><img src={logo} className="w-72" alt="" /></Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-black">
+                    {navLinks}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                {/* https://i.ibb.co/fqgXySz/avater.png */}
+                <div className="dropdown dropdown-end">
+                    {/* {
+                        user ?
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                {
+                                    user.photoURL ?
+                                        <div className="w-10 rounded-full">
+                                            <img src={user.photoURL} alt="https://i.ibb.co/fqgXySz/avater.png" />
+                                        </div>
+                                        :
+                                        <div className="w-10 rounded-full">
+                                            <img src='https://i.ibb.co/fqgXySz/avater.png' alt="" />
+                                        </div>
+                                }
+                            </label>
+                            :
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img src="https://i.ibb.co/ZBXgGBM/user.png" />
+                                </div>
+                            </label>
+                    } */}
+
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-neutral rounded-box w-52">
+                        <li>
+                            {/* {
+                                user ?
+                                    <div className="flex flex-col items-start justify-start text-left">
+                                        <p>Welcome,{user.displayName}</p>
+                                        <button onClick={handleLogOut} className="btn">Log out</button>
+                                    </div>
+                                    :
+                                    <div>
+                                        <Link to='/login'>
+                                            <button className="btn bg-stone-400 font-bold text-black">Login</button>
+                                        </Link>
+                                        <Link to='/register'>
+                                            <button className="btn bg-stone-400 font-bold text-black">Register</button>
+                                        </Link>
+                                    </div>
+                            } */}
+                        </li>
+                    </ul>
+                </div>
+
+
+            </div>
+        </div>
         </>
     );
 };
